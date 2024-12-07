779312df9e465ad53b1de277a2cfaab08c82879a
tdlong <tdlong@uci.edu>
2024-12-07 12:26:56 -0800

    fix haplotype parameter file

--- helpfiles/testing_parameters.R
+++ helpfiles/testing_parameters.R
@@ -27,35 +27,4 @@ ProportionSelect = data.frame(REP=c(1,2,3,4,5,6),Proportion=c(0.113,0.087,0.040,
 # Prefixes must be mapped to C for controls or Z for selected
 TreatmentMapping = data.frame(longTRT=c("Con","Age"),TRT=c("C","Z"))
 
-# step_size (bp)
-# haplotypes will be imputed every step/1000 kb at the kb (each @ 10,20,30,etc)
-step = 10000
-
-# +/- window_size (bp)
-# the windows are centered on the steps above, but of width +/- size/1000 kb
-# if Numflies is generally large (>>200) and seq coverage high I think 50kb is good here
-# but this is for sure a tuning parameter in that bigger windows lead to better
-# haplotype inference, but poorer localization.  Smaller windows lead to better 
-# localization (in theory), but that is offset by poor haplotype inference.  One
-# indication the window is too small is noisy neighbouring -log10p values from
-# the scan.
-size = 50000
-
-# tree cutoff height to claim founders cannot be distinguished from one another
-# this parameter is somewhat mysterious if I am being honest
-# its units are Euclidean distance = sqrt(sum((Fi-Fj)^2) between two founders
-# so over a window of 500 SNPs a distance of <2.5 implies only 
-# 6 SNPs being different, or 1.2% of SNPs, pretty similar haplotypes
-# a cutoff of 5 implies 25 SNPs different, or 5% of SNPs
-# a problem is the distance is not corrected for the number of SNPs, so if a window
-# has 1000 as opposed to 500 SNPs ... then a given distance implies fewer fixed 
-# difference SNPs. I would tend to leave this at 2.5 or perhaps 5.
-h_cutoff=2.5
-
-# file used for Mb->cM mapping
-# we list marker position in Mb (physical) and cM (genetic) coordinates
-# this utilizes a file from flybase for converting physical to genetic
-# it takes place in a function = add_genetic, as there are several such converters
-flymap="helperfiles/flymap.r6.txt"
-
 
