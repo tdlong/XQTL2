95d14233293a6e47947e965f9fdcbfd9b9a1ea13
tdlong <tdlong@uci.edu>
2025-11-17 15:49:03 -0800

    RR dependent window, average Err matrices, smooth LODs and H2's

--- scripts/concat_Chromosome_Scans.Andreas.R
+++ scripts/concat_Chromosome_Scans.Andreas.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(patchwork)
 library(ggplot2)
 library(RColorBrewer)
+library(slider)
 
 source("scripts/XQTL_plotting_functions.R")
 
@@ -15,7 +16,18 @@ files = grep("chr",files,value=TRUE)
 df1 = files %>%
   # read in all the files, appending the path before the filename
   map(~ as_tibble(read.table(file.path(data_path, .)))) %>% 
-  reduce(rbind)
+  reduce(rbind) %>%
+  # Apply sliding window smoothing to summary statistics (5 before, current, 5 after)
+  group_by(chr) %>%
+  arrange(chr, pos) %>%
+  mutate(
+    Wald_log10p = slide_dbl(Wald_log10p, mean, .before = 5, .after = 5),
+    Pseu_log10p = slide_dbl(Pseu_log10p, mean, .before = 5, .after = 5),
+    Falc_H2 = slide_dbl(Falc_H2, mean, .before = 5, .after = 5),
+    Cutl_H2 = slide_dbl(Cutl_H2, mean, .before = 5, .after = 5),
+    avg.var = slide_dbl(avg.var, mean, .before = 5, .after = 5)
+  ) %>%
+  ungroup()
 
 write.table(df1,paste0(data_path, "/",name,".pseudoscan.txt"))
 
